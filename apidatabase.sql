4a94ede3ff16824639ff529d16a81677fb197b34
nayramohamed <nayramohamed706@gmail.com>
2019-04-17 22:07:08 +0200

    Add add_formula

--- apidatabase.sql
+++ apidatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2019 at 01:50 PM
+-- Generation Time: Apr 17, 2019 at 01:51 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 5.6.39
 
@@ -60,11 +60,25 @@ CREATE TABLE `quizzes` (
 --
 
 CREATE TABLE `users` (
-  `name` varchar(1000) COLLATE utf8_croatian_ci NOT NULL,
-  `email` text COLLATE utf8_croatian_ci NOT NULL,
-  `phone_number` int(11) NOT NULL,
-  `interested` text COLLATE utf8_croatian_ci NOT NULL
+  `interested_score` varchar(100) COLLATE utf8_croatian_ci NOT NULL,
+  `age` varchar(100) COLLATE utf8_croatian_ci NOT NULL,
+  `phone_number` varchar(100) COLLATE utf8_croatian_ci NOT NULL,
+  `user_name` varchar(100) COLLATE utf8_croatian_ci NOT NULL,
+  `interested` varchar(300) COLLATE utf8_croatian_ci NOT NULL,
+  `email` varchar(300) COLLATE utf8_croatian_ci NOT NULL,
+  `gender` varchar(200) COLLATE utf8_croatian_ci NOT NULL,
+  `skills` varchar(400) COLLATE utf8_croatian_ci NOT NULL,
+  `address` varchar(400) COLLATE utf8_croatian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`interested_score`, `age`, `phone_number`, `user_name`, `interested`, `email`, `gender`, `skills`, `address`) VALUES
+('10', '10', '10', 'ahmed', 'java', 'ah', 'a', 'a', 'a'),
+('10', '10', '10', 'mohamed', 'java', 'ah', 'ah', 'ah', 'ah'),
+('10', '10', '10', 'nayera', 'english', 'a', 'a', 'a', 'a');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
